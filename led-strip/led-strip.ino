826ea825d346b28c8383f01e221b33a7c1e03bee
Antoine PRONNIER <44138938+FunixG@users.noreply.github.com>
2023-08-02 10:42:31 +0200

    Update led-strip.ino

--- led-strip/led-strip.ino
+++ led-strip/led-strip.ino
@@ -8,11 +8,7 @@
 
 #define BUTTON_WIFI_RESET_PIN 33
 
-#define MQTT_HOST "broker.emqx.io"
-#define MQTT_TOPIC "funix/led/salon"
-#define MQTT_PORT 1883
 WiFiClient client;
-PubSubClient mqttClient(client);
 
 WebServer server(80);
 
@@ -30,9 +26,6 @@ void setup() {
   server.on("/led", HTTP_GET, requestLedColor);
   server.on("/led/off", HTTP_GET, turnOffRequest);
   server.begin();
-
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  mqttClient.setCallback(mqttCallback);
 }
 
 void loop() {
@@ -48,13 +41,6 @@ void loop() {
   }
 
   server.handleClient();
-
-  if (!mqttClient.connected()) {
-    mqttClient.connect("funix/led/salon/ESP_32");
-    mqttClient.subscribe(MQTT_TOPIC);
-  } else {
-    mqttClient.loop();
-  }
 }
 
 enum parsingColor {
@@ -63,45 +49,6 @@ enum parsingColor {
   BLUE,
   NOTHING
 };
-
-void mqttCallback(const char* topic, const byte* message, unsigned int length) {
-  red = "";
-  green = "";
-  blue = "";
-
-  char c = '\0';
-  enum parsingColor parsing;
-
-  for (int i = 0; i < length; ++i) {
-    c = message[i];
-
-    if (c == 'r') {
-      parsing = RED;
-    } else if (c == 'g') {
-      parsing = GREEN;
-    } else if (c == 'b') {
-      parsing = BLUE;
-    } else if (c >= '0' && c <= '9') {
-      if (parsing == RED) {
-        red += c;
-      } else if (parsing == GREEN) {
-        green += c;
-      } else if (parsing == BLUE) {
-        blue += c;
-      }
-    }
-  }
-
-  if (checkIsNumber(red)) {
-    setColor(RED_CHANNEL, red.toInt());
-  }
-  if (checkIsNumber(green)) {
-    setColor(GREEN_CHANNEL, green.toInt());
-  }
-  if (checkIsNumber(blue)) {
-    setColor(BLUE_CHANNEL, blue.toInt());
-  }
-}
 
 void requestLedColor() {
   red = server.arg("r");
